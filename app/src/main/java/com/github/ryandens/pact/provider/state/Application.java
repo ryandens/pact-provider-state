a22a1959d531110b830189904bf6071903d8c0bd
Ryan Dens <ryan.dens@contrastsecurity.com>
2019-10-24 15:25:02 -0400

    :sparkles: support typesafe-config for applicaitonPort

--- app/src/main/java/com/github/ryandens/pact/provider/state/Application.java
+++ app/src/main/java/com/github/ryandens/pact/provider/state/Application.java
@@ -1,8 +1,12 @@
 package com.github.ryandens.pact.provider.state;
 
 import com.sun.net.httpserver.HttpServer;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,9 +15,12 @@ public final class Application {
 
   /** Creates and starts an {@link HttpServer} */
   public static void main(final String[] args) {
+    final var config = config();
+    final var applicationPort = config.getInt("applicationPort");
     final Logger logger = Logger.getLogger("pact-provider-state");
     logger.setLevel(Level.INFO);
-    final InetSocketAddress address = new InetSocketAddress("0.0.0.0", 8080);
+    logger.info("applicationPort:" + applicationPort);
+    final InetSocketAddress address = new InetSocketAddress("0.0.0.0", applicationPort);
     final HttpServer server;
     try {
       server = HttpServer.create(address, 0);
@@ -36,5 +43,24 @@ public final class Application {
     logger.info("HttpServer started listening on" + address);
 
     server.start();
+  }
+
+  /**
+   * Shamelessly stolen from <a
+   * href="https://github.com/gilday/how-to-microservice/blob/master/src/main/java/com/johnathangilday/App.java">gilday/how-to-microservice</a>
+   */
+  private static Config config() {
+    final var defaultConfig = ConfigFactory.load();
+    final var externalConfigFile = Optional.ofNullable(System.getProperty("config")).map(File::new);
+
+    if (externalConfigFile.isPresent() && !externalConfigFile.get().exists()) {
+      throw new RuntimeException(
+          "external config file " + externalConfigFile.get().getAbsolutePath() + " not found");
+    }
+
+    return externalConfigFile
+        .map(ConfigFactory::parseFile)
+        .map(c -> c.withFallback(defaultConfig))
+        .orElse(defaultConfig);
   }
 }
