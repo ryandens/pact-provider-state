21f759b365714d70b7046d6dcc11a7314d55a370
Ryan Dens <ryan.dens@contrastsecurity.com>
2019-10-24 19:49:21 -0400

    :heavy_plus_sign: add mockito as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ allprojects {
 
     dependencies {
         implementation("mysql", "mysql-connector-java", "5.1.6")
+        testImplementation("org.mockito:mockito-core:2.25.0")
         testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.4.0")
         testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.4.0")
     }
