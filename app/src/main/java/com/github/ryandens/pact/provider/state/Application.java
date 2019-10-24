3ff6e981bface6590fc7fc517db7772106d9691a
Ryan Dens <ryan.dens@contrastsecurity.com>
2019-10-24 16:50:16 -0400

    :sparkles: add ability to create connection to database

--- app/src/main/java/com/github/ryandens/pact/provider/state/Application.java
+++ app/src/main/java/com/github/ryandens/pact/provider/state/Application.java
@@ -6,6 +6,9 @@ import com.typesafe.config.ConfigFactory;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,18 +18,32 @@ public final class Application {
 
   /** Creates and starts an {@link HttpServer} */
   public static void main(final String[] args) {
+    // read in config
     final var config = config();
-    final var applicationPort = config.getInt("applicationPort");
-    final Logger logger = Logger.getLogger("pact-provider-state");
-    logger.setLevel(Level.INFO);
-    logger.info("applicationPort:" + applicationPort);
-    final InetSocketAddress address = new InetSocketAddress("0.0.0.0", applicationPort);
+    // set log level from config
+    logger.setLevel(Level.parse(config.getString("logLevel")));
+
+    // create a server which will listen on localhost with the application port read from config
+    final InetSocketAddress address =
+        new InetSocketAddress("0.0.0.0", config.getInt("applicationPort"));
     final HttpServer server;
     try {
       server = HttpServer.create(address, 0);
     } catch (IOException e) {
       throw new RuntimeException("Problem initializing HttpServer", e);
     }
+
+    // create a connection to the database
+    final var connection =
+        getConnection(
+            "jdbc:mysql://"
+                + config.getString("mysqlHost")
+                + ":"
+                + config.getString("mysqlPort")
+                + "/"
+                + config.getString("mysqlPath"),
+            config.getString("mysqlUser"),
+            config.getString("mysqlPassword"));
 
     server.createContext(
         "/ping",
@@ -63,4 +80,21 @@ public final class Application {
         .map(c -> c.withFallback(defaultConfig))
         .orElse(defaultConfig);
   }
+
+  /**
+   * Creates a connection to the database
+   *
+   * @param url connection url of the format "jdbc:subprotocol:subname"
+   */
+  private static Connection getConnection(
+      final String url, final String user, final String password) {
+    try {
+      return DriverManager.getConnection(url, user, password);
+    } catch (SQLException e) {
+      logger.info("Unable to create Connection, continuing without connection to the database");
+      return null;
+    }
+  }
+
+  private static final Logger logger = Logger.getLogger("pact-provider-state");
 }
