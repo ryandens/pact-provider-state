e00252900eb715c77041ef77af7935ecbb9bb211
Ryan Dens <ryan.dens@contrastsecurity.com>
2019-10-25 17:06:51 -0400

    :recycle: equals and hashCode on value object

--- app/src/main/java/com/github/ryandens/pact/provider/state/ProviderState.java
+++ app/src/main/java/com/github/ryandens/pact/provider/state/ProviderState.java
@@ -1,6 +1,7 @@
 package com.github.ryandens.pact.provider.state;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
 
 /**
  * Value object to hold information about a <a
@@ -14,11 +15,31 @@ final class ProviderState {
   @JsonProperty("action")
   private String action;
 
+  public ProviderState(final String state, final String action) {
+    this.state = state;
+    this.action = action;
+  }
+
+  public ProviderState() {}
+
   String state() {
     return state;
   }
 
   String action() {
     return action;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    final ProviderState that = (ProviderState) o;
+    return Objects.equals(state, that.state) && Objects.equals(action, that.action);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(state, action);
   }
 }
