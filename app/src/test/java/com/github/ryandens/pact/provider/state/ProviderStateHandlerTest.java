d780d97dfa14c698e98d851d01ef0189555aee5d
Ryan Dens <ryan.dens@contrastsecurity.com>
2019-10-25 15:10:21 -0400

    :white_check_mark: add parameterized test cases

--- app/src/test/java/com/github/ryandens/pact/provider/state/ProviderStateHandlerTest.java
+++ app/src/test/java/com/github/ryandens/pact/provider/state/ProviderStateHandlerTest.java
@@ -11,17 +11,19 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /** Unit tests for the {@link ProviderStateHandler} */
-class ProviderStateHandlerTest {
+final class ProviderStateHandlerTest {
 
   private ProviderStateHandler providerStateHandler;
-  private Connection connectionStub;
   private Statement statementStub;
-  private String setupQuery;
-  private String teardownQuery;
+  private static final String setupQuery = "INSERT into users(name,age) VALUES('Ryan', 23);";
+  private static final String teardownQuery = "DELETE from users WHERE name = 'Ryan';";
 
   /**
    * Sets up the stubs for and the instance of {@link ProviderStateHandler}
@@ -31,12 +33,10 @@ class ProviderStateHandlerTest {
   @BeforeEach
   void beforeEach() throws SQLException {
     // stub SQL Connection which returns a statement
-    connectionStub = stub(Connection.class);
+    final var connectionStub = stub(Connection.class);
     // Statement mock used to verify
     statementStub = stub(Statement.class);
     when(connectionStub.createStatement()).thenReturn(statementStub);
-    setupQuery = "INSERT into users(name,age) VALUES('Ryan', 23);";
-    teardownQuery = "DELETE from users WHERE name = 'Ryan';";
     providerStateHandler = new ProviderStateHandler(connectionStub, setupQuery, teardownQuery);
   }
 
@@ -44,9 +44,15 @@ class ProviderStateHandlerTest {
     return mock(classToStub, withSettings().stubOnly());
   }
 
+  static Stream<Arguments> actionAndQueryProvider() {
+    return Stream.of(Arguments.of("setup", setupQuery), Arguments.of("teardown", teardownQuery));
+  }
+
   /** Verifies that the {@link ProviderStateHandler} properly handles a setup message */
-  @Test
-  void it_deserializes_valid_body_and_sends_setupQuery() {
+  @ParameterizedTest(name = "action={0}, query={1}")
+  @MethodSource("actionAndQueryProvider")
+  void it_deserializes_valid_body_and_sends_setupQuery(
+      final String action, final String expectedQuery) {
     // GIVEN a mocked HttpExchange
     final var httpExchange = mock(HttpExchange.class);
 
@@ -54,12 +60,12 @@ class ProviderStateHandlerTest {
     when(httpExchange.getRequestBody())
         .thenReturn(
             new ByteArrayInputStream(
-                "{\"state\": \"jane is the best\", \"action\": \"setup\"}".getBytes()));
+                ("{\"state\": \"jane is the best\", \"action\": \"" + action + "\"}").getBytes()));
 
     // AND GIVEN the Statement returns one when the update  is executed with the setupQuery
     try {
       // simple stub to make the
-      when(statementStub.executeUpdate(setupQuery)).thenReturn(1);
+      when(statementStub.executeUpdate(expectedQuery)).thenReturn(1);
     } catch (SQLException ignored) {
     }
 
